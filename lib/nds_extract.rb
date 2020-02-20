8860202d19fcc4ff36bd5e690ec2d5ac4f8ee5ff
Joshua Gauthreaux <joshuagauthreaux@gmail.com>
2020-02-20 02:55:55 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -8,18 +8,14 @@ def directors_totals(nds)
   
   row_index = 0 
   while nds[row_index]  do 
-    # cycles through each director's info
     direc = nds[row_index][:name]
-    # assigns director name to direc, updates each cycle
-   
+    
     key_value = 0 
     d_ww_g = 0 
     while key_value < nds[row_index][:movies].length do
-      # cycles through each movie for selected director. 
-      
       d_ww_g += nds[row_index][:movies][key_value][:worldwide_gross]
       key_value += 1 
-      #sums :worldwide_gross from each title into d_ww_g
+     
   end
   result[direc] = d_ww_g
   row_index += 1 
